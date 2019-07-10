df02c25d38f23322f18a16c51aac49a826f05c1a
Juliano Fernandes <julianofernandes@gmail.com>
2019-07-10 20:54:52 -0300

    say hello with c

---
+++ c/src/hello_world.c
@@ -0,0 +1,6 @@
+#include "hello_world.h"
+
+const char *hello_world(void)
+{
+	return "Hello, World!";
+}
---
+++ c/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "hello_world.h"
+
+int main(void)
+{
+	printf("%s\n", hello_world());
+}
