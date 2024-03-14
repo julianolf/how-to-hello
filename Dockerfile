e5999d04c75ee9a6c93ff34b4065bfb47fc860a1
Juliano Fernandes <julianofernandes@gmail.com>
2024-03-14 20:38:09 -0300

    update container image and entrypoint script to run csharp code

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM alpine:3.19
 
-RUN apk add --no-cache clang make
+RUN apk add --no-cache clang make dotnet7-sdk
 
 ENV CC=clang
+ENV DOTNET_NOLOGO=true
 
 WORKDIR /opt/hello
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 set -e
 
 echo "Saying Hello in C"
-cd c/ && make run && cd - >/dev/null
+cd c/ && make run && cd - >/dev/null && echo
+
+echo "Saying Hello in C#"
+cd csharp/ && dotnet run --project HelloWorld && cd - >/dev/null && echo
